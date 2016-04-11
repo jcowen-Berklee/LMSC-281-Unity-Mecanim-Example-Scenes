551109b9b20df3cb5f2a4155578b4230739e2be3
Bernard Duc <Bernard@150-STU-WRLS-172-30-8-208.berklee.net>
2016-04-11 12:12:30 -0400

    Crowd Sim Audio done
    Added three random footsteps to the clones in the Crowd Sim scene using
    an AudioClip[] array. The teddy bear has a new script (copy of the
    original script). Now it’s RandomTeddyBear instead of RandomCharacter.

---
+++ Assets/Scripts/CrowdSound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowdSound : MonoBehaviour {
+
+	public AudioClip[] footSteps; //make an arrayed variable (to attach more than one sound)
+	private AudioSource steps;
+
+	void Start ()
+	{
+		//Loading the items into the array
+		footSteps =  new AudioClip[]{(AudioClip)Resources.Load("SFX/steps1.wav"),
+			(AudioClip)Resources.Load("SFX/steps2.wav"), 
+			(AudioClip)Resources.Load("SFX/steps3.wav")};
+	}
+
+	// Play random sound from footSetps audio Array
+	void PlaySound()
+	{
+		steps = GetComponent<AudioSource> ();
+		int ran = Random.Range (0, footSteps.Length);
+		steps.clip = footSteps [ran]; 
+		steps.Play ();
+	}
+}
