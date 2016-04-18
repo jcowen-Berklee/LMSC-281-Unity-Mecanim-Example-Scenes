9b615abbee733368f594eda339bcdde8cf9c4e6b
andrewjohnperrin <andrewperrin@Andrews-MacBook-Pro-3.local>
2016-04-18 19:33:05 -0400

    added some bear grunt sounds to resources

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class AudioManager : MonoBehaviour {
 
 	public static AudioClip[] footSteps;
+	public static AudioClip[] bearDying;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +13,14 @@ public class AudioManager : MonoBehaviour {
 			Resources.Load<AudioClip>("Audio/SFX/steps2"),
 			Resources.Load<AudioClip>("Audio/SFX/steps3")
 		};
+
+		bearDying = new AudioClip[] {
+
+			Resources.Load<AudioClip>("Audio/SFX/BigGrunt"),
+			Resources.Load<AudioClip>("Audio/SFX/SmallGrunt")
+
+		};
+
 	}
 
 	//Plays a random sound from an AudioClip array
